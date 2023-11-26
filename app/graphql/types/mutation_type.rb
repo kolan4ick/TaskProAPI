c8181184b0a981466b7b51ec22f96210854d16c6
kolan4ick <Kolan4ick@gmail.com>
2023-11-26 02:05:27 +0200

    Replaced the list model with a roster model for clarity and readability in our application's design. Changes include renaming files and classes from 'list' to 'roster', adjusting associations in the Task, Board, and Roster models accordingly, and refining the GraphQL schema to reflect these changes. The migration files to create the rosters table and add a foreign key reference to tasks were also adjusted. Test files also reflect the name changes to ensure testing continuity. Refactor list model to roster model

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,20 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :delete_comment, mutation: Mutations::Comments::DeleteComment
+    field :create_comment, mutation: Mutations::Comments::CreateComment
+    field :delete_task, mutation: Mutations::Tasks::DeleteTask
+    field :update_task, mutation: Mutations::Tasks::UpdateTask
+    field :create_task, mutation: Mutations::Tasks::CreateTask
+    field :delete_roster, mutation: Mutations::Rosters::DeleteRoster
+    field :update_roster, mutation: Mutations::Rosters::UpdateRoster
+    field :create_roster, mutation: Mutations::Rosters::CreateRoster
+    field :delete_board, mutation: Mutations::Boards::DeleteBoard
+    field :update_board, mutation: Mutations::Boards::UpdateBoard
+    field :create_board, mutation: Mutations::Boards::CreateBoard
+    field :delete_project, mutation: Mutations::Projects::DeleteProject
+    field :update_project, mutation: Mutations::Projects::UpdateProject
+    field :create_project, mutation: Mutations::Projects::CreateProject
     field :users_update, mutation: Mutations::Users::UpdateUser
     field :sign_up, mutation: Mutations::Users::SignUpUser
     field :sign_in, mutation: Mutations::Users::SignInUser
