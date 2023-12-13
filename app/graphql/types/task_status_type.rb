28945a34a5c4ec715fa524d7448cb8a058486f6f
Yuryshynets Mykola <kolan4ick@gmail.com>
2023-12-13 18:30:07 +0200

    Update status enum for tasks

--- app/graphql/types/task_status_type.rb
+++ app/graphql/types/task_status_type.rb
@@ -4,8 +4,8 @@ module Types
   class TaskStatusType < Types::BaseEnum
     description "Task status enum"
 
-    value 'not_started', 'Task has not been started', value: 'not_started'
-    value 'in_progress', 'Task is in progress', value: 'in_progress'
-    value 'completed', 'Task has been completed', value: 'completed'
+    value :not_started, 'Task has not been started', value: 0
+    value :in_progress, 'Task is in progress', value: 1
+    value :completed, 'Task has been completed', value: 2
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ApplicationRecord
   belongs_to :assignee, class_name: 'User', foreign_key: 'assignee_id', optional: true
   has_many :comments, dependent: :destroy
 
-  enum status: { not_started: "not_started", in_progress: "in_progress", completed: "completed" }
+  enum status: { not_started: 0, in_progress: 1, completed: 2 }
 
   enum priority_level: { low: 0, medium: 1, high: 2 }
   belongs_to :roster
