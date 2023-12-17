b63e238ddf9224472cc913f0971079e1d23cfed5
kolan4ick <Kolan4ick@gmail.com>
2023-12-17 22:10:55 +0200

    Add user for comment type

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -5,6 +5,7 @@ module Types
     field :id, ID, null: false
     field :content, String
     field :user_id, Integer, null: false
+    field :user, Types::UserType, null: false
     field :task_id, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
